669df4c59bd9e65579d10c5ade363d3291570a24
shuajay <joshuajayphiri5@gmail.com>
2022-09-22 14:42:49 +0200

    tweaked main.py

--- main.py
+++ main.py
@@ -99,15 +99,3 @@ def addBook_post():
     # display the message if the book has been successfully added
     flash('Book successfully added')
     return redirect(url_for('main.addBook'))
-
-
-#custom error pages
-#invalid page
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-#internal server error
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template("500.html"), 500
